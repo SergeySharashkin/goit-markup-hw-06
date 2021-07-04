1b1b06ea25caa82fa196a93587fdce9e57a0c344
SergeySharashkin <S_sharashkin@mail.ru>
2021-07-04 23:44:34 +0300

    о

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
@@ -12,19 +12,5 @@
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
     }
-=======
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
->>>>>>> aac34cc47f7c593cfd1e7f6dc8d38b9b0065bfd0
-  })();
+
+
